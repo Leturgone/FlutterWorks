245ee51709c41b42be3485455a0b0290d4b01a18
Leturgone <finekon221@gmail.com>
2025-10-25 03:26:26 +0300

    Добавлен MainScaffold в экран галереи

--- work7/lib/features/gallery/screens/comic_gallery_screen.dart
+++ work7/lib/features/gallery/screens/comic_gallery_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:work7/main_scaffold.dart';
 
 import '../widgets/comic_gallery_widget.dart';
 
@@ -8,8 +9,8 @@ class ComicGalleryScreen extends StatelessWidget {
   const ComicGalleryScreen({super.key, required this.imageUrls});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: ComicGalleryWidget(imageUrls: imageUrls)
+    return MainScaffold(
+        body: ComicGalleryWidget(imageUrls: imageUrls), currentIndex: 2,
     );
   }
 }
