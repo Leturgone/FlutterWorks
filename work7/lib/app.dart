7927fb549d513c4fa2c121f5d45b0e18ae5fb713
Leturgone <finekon221@gmail.com>
2025-11-01 03:31:47 +0300

    Добавлен роутер в апп

--- work7/lib/app.dart
+++ work7/lib/app.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:work7/home_page.dart';
+import 'package:work7/router.dart';
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: HomePage()
+    return MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        routerConfig: appRouter
     );
   }
 }
