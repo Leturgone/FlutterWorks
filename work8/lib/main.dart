ad3dd18e363a4203734285fd71376b0c7ce07de4
Leturgone <finekon221@gmail.com>
2025-11-01 10:47:44 +0300

    Инициализация DI

--- work8/lib/main.dart
+++ work8/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:work8/features/impression_note/repository/impression_notes_repository.dart';
 
 import 'app.dart';
 
 void main() {
+  GetIt.I.registerSingleton<ImpressionNoteRepository>(ImpressionNoteRepository());
   runApp(MyApp());
 }
