14c5dd47077193600fadab9bd8509c5ba0e0fbf1
Leturgone <finekon221@gmail.com>
2025-11-01 10:47:12 +0300

    Удаление провадйера

--- work8/lib/app.dart
+++ work8/lib/app.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:work8/features/impression_note/impression_rep_provider.dart';
-import 'package:work8/features/impression_note/repository/impression_notes_repository.dart';
 import 'package:work8/router.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    final impressionNoteRepository = ImpressionNoteRepository();
-    return ImpressionRepProvider(
-        impressionNoteRepository: impressionNoteRepository,
-        child: MaterialApp.router(
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            ),
-            routerConfig: appRouter
-        ));
+    return MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        routerConfig: appRouter
+    );
   }
 }
