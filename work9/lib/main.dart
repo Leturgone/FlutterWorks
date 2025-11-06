7c785d03828baa1f53e9ba218783ae3cc2d61342
Leturgone <finekon221@gmail.com>
2025-11-06 19:28:42 +0300

    Класс сохранения добавлен в DI

--- work9/lib/main.dart
+++ work9/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:work9/features/impression_note/repository/impression_notes_repository.dart';
+import 'package:work9/features/impression_note/state/impression_notes_store.dart';
 
 import 'app.dart';
 
 void main() {
-  GetIt.I.registerSingleton<ImpressionNoteRepository>(ImpressionNoteRepository());
+  GetIt.I.registerSingleton<ImpressionNotesStore>(ImpressionNotesStore());
   runApp(MyApp());
 }
