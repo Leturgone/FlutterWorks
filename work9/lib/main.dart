ec18648bf16b02fdf96a7626396454aa2ac2ae7f
Leturgone <finekon221@gmail.com>
2025-11-15 02:31:06 +0300

    Сделан di в main

--- work9/lib/main.dart
+++ work9/lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:work9/features/impression_note/state/impression_notes_store.dart';
+import 'package:mobx/mobx.dart';
 
 import 'app.dart';
+import 'features/impression_note/models/impression_note.dart';
 
 void main() {
-  GetIt.I.registerSingleton<ImpressionNotesStore>(ImpressionNotesStore());
+  ObservableList<ImpressionNote> impressionNotes = ObservableList.of([
+    ImpressionNote(
+      id: 0,
+      seriesImage: "https://i.annihil.us/u/prod/marvel/i/mg/1/d0/519bad24bebcd.jpg",
+      description: "Запись",
+    ),
+  ]);
+
+  GetIt.I.registerSingleton<ObservableList<ImpressionNote>>(impressionNotes);
+
   runApp(MyApp());
 }
