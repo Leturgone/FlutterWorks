47dce597f4e78d890b6b0680b048e96a91bfead9
Leturgone <finekon221@gmail.com>
2025-11-22 02:54:37 +0300

    Добавлены виджеты для экрана игр

---
+++ work10/lib/features/games/widgets/game_about_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+import '../models/game.dart';
+
+class GameAboutView extends StatelessWidget {
+  final Game game;
+
+  const GameAboutView({super.key, required this.game});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(child:
+          CachedNetworkImage(
+            imageUrl: game.image,
+            width: 150,
+            height: 200,
+            fit: BoxFit.cover,
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          )),
+          SizedBox(height: 16),
+          Text(
+            game.title,
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 8),
+          Row(
+            children: [
+              Icon(Icons.developer_mode, size: 16),
+              SizedBox(width: 4),
+              Text('Разработчик: ${game.developer}'),
+            ],
+          ),
+          SizedBox(height: 4),
+          Row(
+            children: [
+              Icon(Icons.computer, size: 16),
+              SizedBox(width: 4),
+              Text('Платформа: ${game.platform}'),
+            ],
+          ),
+          SizedBox(height: 16),
+          Text(
+            game.description ?? 'Нет описания',
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ work10/lib/features/games/widgets/games_list_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+import '../models/game.dart';
+import 'games_tile.dart';
+
+class GameListView extends StatelessWidget {
+  final List<Game> gameList;
+  final ValueChanged<Game> onGameTap;
+
+  const GameListView({
+    super.key,
+    required this.gameList,
+    required this.onGameTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (gameList.isEmpty) {
+      return const Center(
+        child: Text(
+          'Список игр пуст',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: gameList.length,
+      itemBuilder: (context, index) {
+        final game = gameList[index];
+        return GameTile(
+          key: ValueKey(game.id),
+          game: game,
+          onTap: onGameTap,
+        );
+      },
+    );
+  }
+}
