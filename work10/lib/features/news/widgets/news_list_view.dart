5e34fcfdc31aecba7455fa8104a49a6e1d6c0e59
Leturgone <finekon221@gmail.com>
2025-11-22 02:43:05 +0300

    Добавлены вилжеты для экрана с новостями

---
+++ work10/lib/features/news/widgets/news_list_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../models/news.dart';
+import 'news_tile.dart';
+
+class NewsListView extends StatelessWidget {
+  final List<News> newsList;
+  final ValueChanged<News> onNewsTap;
+
+  const NewsListView({
+    super.key,
+    required this.newsList,
+    required this.onNewsTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (newsList.isEmpty) {
+      return const Center(
+        child: Text(
+          'Список новостей пуст',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: newsList.length,
+      itemBuilder: (context, index) {
+        final news = newsList[index];
+        return NewsTile(
+          key: ValueKey(news.id),
+          news: news,
+          onTap: onNewsTap,
+        );
+      },
+    );
+  }
+}
