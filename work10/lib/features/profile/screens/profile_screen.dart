7e4d547af8d599739ce4135800a3663db1f9c8af
Leturgone <finekon221@gmail.com>
2025-11-22 03:05:12 +0300

    Добавлен экран для бизнес экрана профиля

---
+++ work10/lib/features/profile/screens/profile_screen.dart
@@ -0,0 +1,41 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:work10/features/profile/stores/profile_store.dart';
+import 'package:work10/features/profile/widgets/features_view.dart';
+import 'package:work10/features/profile/widgets/profile_view.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final ProfileStore store = ProfileStore();
+
+  ProfileScreen({super.key}){
+    store.loadUserData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Профиль'),
+      ),
+      body: Column(
+        children: [
+          Observer(
+            builder: (_) {
+              if (store.isLoading) {
+                return Center(child: CircularProgressIndicator());
+              }
+              final user = store.user;
+              if (user == null) {
+                return Center(child: Text('Нет данных'));
+              }
+              return ProfileView(user: user);
+            },
+          ),
+          FeaturesView()
+        ],
+      ),
+    );
+  }
+}
