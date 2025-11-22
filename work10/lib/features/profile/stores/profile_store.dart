bab8ffdf666cabdd8324d86f87678c63f4113b4d
Leturgone <finekon221@gmail.com>
2025-11-22 03:04:49 +0300

    Добавлен стор для бизнес экрана профиля

---
+++ work10/lib/features/profile/stores/profile_store.dart
@@ -0,0 +1,26 @@
+
+import '../models/user.dart';
+import 'package:mobx/mobx.dart';
+part 'profile_store.g.dart';
+
+class ProfileStore = _ProfileStore with _$ProfileStore;
+
+abstract class _ProfileStore with Store {
+  @observable
+  User? user;
+
+  @observable
+  bool isLoading = false;
+
+  @action
+  Future<void> loadUserData() async {
+    isLoading = true;
+    await Future.delayed(Duration(seconds: 1));
+    user = User(
+      id: 1,
+      username: 'comic_fan',
+      notesCount: 15,
+    );
+    isLoading = false;
+  }
+}
