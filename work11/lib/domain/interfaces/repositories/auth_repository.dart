4d2eed55b59b32611ace9a39d4f6c200e8fdf2b2
Leturgone <finekon221@gmail.com>
2025-12-19 09:40:36 +0300

    Добавлен метод получения профиля

--- work11/lib/domain/interfaces/repositories/auth_repository.dart
+++ work11/lib/domain/interfaces/repositories/auth_repository.dart
@@ -4,4 +4,5 @@ import 'package:work11/core/models/user.dart';
 abstract class AuthRepository {
   Future<User> login(String login, password);
   Future<User> register(String name, String login, String password);
+  Future<User> getProfile(int id);
 }
